1c1528a2538b709125bbba24e7610956b42be4f6
kristjanpeterson <kristjanpeterson1@gmail.com>
2021-09-17 13:24:38 +0300

    Remove ERC721 extension and jsut leave ERC721URIStorage extension

--- contracts/NftInjectableTokenId.sol
+++ contracts/NftInjectableTokenId.sol
@@ -1,11 +1,10 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract NFTTokenId is ERC721, ERC721URIStorage, Ownable {
+contract NftInjectableTokenId is ERC721URIStorage, Ownable {
     constructor(string memory _name, string memory _symbol) ERC721(_name, _symbol) {}
 
     function safeMint(address to, uint256 tokenId) public onlyOwner {
@@ -20,20 +19,5 @@ contract NFTTokenId is ERC721, ERC721URIStorage, Ownable {
         _setTokenURI(tokenId, tokenURI);
 
         return tokenId;
-    }
-
-    // The following functions are overrides required by Solidity.
-
-    function _burn(uint256 tokenId) internal override(ERC721, ERC721URIStorage) {
-        super._burn(tokenId);
-    }
-
-    function tokenURI(uint256 tokenId)
-    public
-    view
-    override(ERC721, ERC721URIStorage)
-    returns (string memory)
-    {
-        return super.tokenURI(tokenId);
     }
 }
