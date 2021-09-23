bdcc58812f111ddaaada0e7153741f377c9bf594
kristjanpeterson <kristjanpeterson1@gmail.com>
2021-09-23 10:10:40 +0300

    Add accesscontrol to nft contract

---
+++ contracts/NftInjectableTokenIdRoles.sol
@@ -0,0 +1,35 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import "@openzeppelin/contracts/access/AccessControl.sol";
+
+contract NftInjectableTokenIdRoles is ERC721URIStorage, AccessControl {
+    bytes32 public constant MINTER_ROLE = keccak256("MINTER_ROLE");
+
+    constructor(string memory _name, string memory _symbol, address owner) ERC721(_name, _symbol) {
+        _setupRole(DEFAULT_ADMIN_ROLE, owner);
+        _setupRole(MINTER_ROLE, owner);
+        _setupRole(MINTER_ROLE, msg.sender);
+    }
+
+
+    function mintToCaller(address caller, uint256 tokenId, string memory tokenURI)
+    public onlyRole(MINTER_ROLE)
+    returns (uint256)
+    {
+        _safeMint(caller, tokenId);
+        _setTokenURI(tokenId, tokenURI);
+        return tokenId;
+    }
+
+
+    function supportsInterface(bytes4 interfaceId)
+    public
+    view
+    override(ERC721, AccessControl)
+    returns (bool)
+    {
+        return ERC721.supportsInterface(interfaceId);
+    }
+}
