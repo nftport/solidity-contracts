55d5277b1d548147a8c821b6c07b4f7cec385eb2
patelvishal1401 <patelvishal1401@gmail.com>
2021-11-17 14:00:51 +0530

    fix:ERC1155 lint errors

--- contracts/ERC1155NFT.sol
+++ contracts/ERC1155NFT.sol
@@ -14,13 +14,12 @@ contract ERC1155NFT is ERC1155, Ownable {
 
     string public name;
     string public symbol;
-
     mapping (uint256 => uint256) public tokenSupply;
     mapping (uint256 => string) customUri;
 
     constructor(string memory _uri, string memory _name, string memory _symbol) ERC1155(_uri) {
-          name = _name;
-          symbol = _symbol;
+        name = _name;
+        symbol = _symbol;
     }
 
     function setURI(string memory newuri) public onlyOwner {
@@ -36,33 +35,33 @@ contract ERC1155NFT is ERC1155, Ownable {
         return tokenSupply[_id];
     }
 
-    function uri(uint256 _id) override public view returns (string memory) {
-          bytes memory customUriBytes = bytes(customUri[_id]);
+    function uri(uint256 _id) public override view returns (string memory) {
+        bytes memory customUriBytes = bytes(customUri[_id]);
 
-          if (customUriBytes.length > 0) {
-              return customUri[_id];
-          } else {
-              return super.uri(_id);
-          }
+        if (customUriBytes.length > 0) {
+            return customUri[_id];
+        } else {
+            return super.uri(_id);
+        }
     }
 
     function mint( address account, uint256 id, uint256 amount, bytes memory data, string memory uri) public onlyOwner {
-            if (bytes(uri).length > 0) {
-                customUri[id] = uri;
-                emit URI(uri, id);
-            }
-            _mint(account, id, amount, data);
-            tokenSupply[id] = tokenSupply[id].add(amount);
+        if (bytes(uri).length > 0) {
+            customUri[id] = uri;
+            emit URI(uri, id);
+        }
+        _mint(account, id, amount, data);
+        tokenSupply[id] = tokenSupply[id].add(amount);
 
-            for (uint256 i = 0; i < owners[id].length; i++) {
-                  if (owners[id][i] == account) {
-                      return;
-                  }
+        for (uint256 i = 0; i < owners[id].length; i++) {
+            if (owners[id][i] == account) {
+                return;
             }
-            owners[id].push(account);
+        }
+        owners[id].push(account);
     }
 
     function ownersCorrespondingToToken(uint256 index) public view returns (address[] memory){
-            return owners[index];
+        return owners[index];
     }
 }
